cf1b40a87480c5ba1d2256123073c1c5d3a57bcd
samvelarakelyan <62077195+samvelarakelyan@users.noreply.github.com>
2020-03-13 01:46:28 +0400

    Add files via upload

---
+++ Homeworks/Homework1/h1.py
@@ -0,0 +1,73 @@
+#PROBLEM 1
+
+
+#h1-1
+project='cake'
+
+#h1-2
+difficulty=5
+
+#h1-3
+ingredients=['flour','butter','sugar','eggs','cocoa powder','baking powder']
+
+#h1-4
+print('apple' in ingredients)
+
+#h1-5
+print('butter' in ingredients)
+
+#h1-6
+print('eggs' in ingredients or 'margarine' in ingredients)
+
+#h1-7
+print('eggs' in ingredients and 'margarine' in ingredients)
+
+#h1-8
+flour =175
+butter =175
+sugar ='100G'
+eggs =2
+cocoa_powder ='1ts'
+baking_powder =0.5
+
+#h1-9
+print('flour-',flour)
+print('butter-',butter)
+print('suger-'+sugar)
+print('eggs-',eggs)
+print('cocoa_pownder-'+cocoa_powder)
+print('baking_powder-',baking_powder)
+
+
+
+
+#PROBLEM 3
+
+
+a=15
+b=8
+c=2
+
+result1=5*(a**2)-a*b+(a%2)-a/5
+result2=b**3+3*a*b-10*c
+
+print('result 1 =',result1)
+print('result 2 =',result2)
+
+
+
+
+#PROBLEM4
+
+
+answer=int(input('Enter one integer, please:'))
+
+if(answer%2==0):
+    print('This number is even')
+else:
+    print('This number is odd')
+
+
+
+
+
---
+++ Practicals/practical1/p1.py
@@ -0,0 +1,47 @@
+#PROBLEM 2
+
+#p2-1
+course='Intro to python'
+
+#p2-2
+student='Samvel Arakelyan'
+
+#p2-3
+nickname='Moso'
+
+#p2-4
+grade=100
+
+#p2-5
+python_expert=True
+
+#p2-6
+print('cource:',course)
+print('studand:',student)
+print('nickname:',nickname)
+print('grade:',grade)
+print('python_expert',python_expert)
+
+#-------------------------------------------------
+
+
+#PROBLEM 4
+
+
+#p4-1
+AB=3
+AC=4
+
+#p4-2
+BC=(AB**2+AC**2)**(1/2)
+
+#P4-3
+print('The hypotenuse of the triangle ABC​ =',BC)
+
+
+
+
+
+
+
+
