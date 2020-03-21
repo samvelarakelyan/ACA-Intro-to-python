45813f77be3743bebe3a58aa527af6e3dd40b9a2
samvelarakelyan <62077195+samvelarakelyan@users.noreply.github.com>
2020-03-21 12:56:33 +0400

    Delete problem6.py

--- Practicals/Practical2/problem6.py
+++
@@ -1,9 +0,0 @@
-text1=input("Enter please something word:")
-
-a=text1.count("a")
-b=text1.count("b")
-c=text1.count("c")
-d=text1.count("d")
-e=text1.count("e")
-
-print("# of a:",a,"\n# of b:",b,"\n# of c:",c,"\n# of d:",d,"\n# of e:",e)
