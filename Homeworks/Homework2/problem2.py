ebf3fad0b7de82805345d6dfe227924be6e69ab3
samvelarakelyan <62077195+samvelarakelyan@users.noreply.github.com>
2020-04-01 18:16:57 +0400

    Update problem2.py

--- Homeworks/Homework2/problem2.py
+++ Homeworks/Homework2/problem2.py
@@ -1,14 +1,13 @@
 
-
 text=input("Enter please text,which has seven or more odd characters։")
 
 start=len(text)//2-1
 end=start+3
 mid3chars=text[start:end]
-text_upper=text.upper()
+new_text=text[:start]+mid3chars.upper()+text[end:]
 
 print("The old string:",text)
 print("Middle 3 characters:",mid3chars)
-print("The new string:",text_upper)
+print("The new string:",new_text)
 
 
